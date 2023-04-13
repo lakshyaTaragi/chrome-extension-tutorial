7bcf08e7584ed32d0474ad896372cfb64587f1fe
Lakshya Taragi <lakshya.taragi01@gmail.com>
2023-04-13 12:42:32 +0530

    Implement "addNewBookmarkEventHandler"

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,8 @@
 (() => {
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
+    let currentVideoBookmarks = [];
+
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, value, videoId } = obj;
 
@@ -9,4 +11,47 @@
             newVideoLoaded();
         }
     });
+
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        console.log(bookmarkBtnExists);
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
+            
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+            youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+
+        }
+    }
+
+    const addNewBookmarkEventHandler = () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            time: currentTime,
+            desc: "Bookmark at " + getTime(currentTime),
+        };
+        console.log(newBookmark);
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify(
+                [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+            )
+        });
+    }
+
+    newVideoLoaded();
+
 })();
+
+const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(t);
+    return date.toISOString().substr(11, 8);
+}
